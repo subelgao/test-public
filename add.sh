2a844826db60ad01a46b6608c59eeba457574f8b
subelgao <surajnapster@gmail.com>
2024-06-29 15:19:02 +0530

    Updated add.sh for third number.

--- add.sh
+++ add.sh
@@ -4,7 +4,9 @@ echo "Enter the first number:"
 read NUM1
 echo "Enter the second number:"
 read NUM2
+echo "Enter the third number:"
+read NUM3
 
-SUM=$((NUM1 + NUM2))
+SUM=$((NUM1 + NUM2 + NUM3))
 
-echo "The sum of $NUM1 and $NUM2 is $SUM."
+echo "The sum of $NUM1,$NUM2 and $NUM3 is $SUM."
